ad08d7a0ebded682f024c7fa4fb7245f1eb1687a
Mackenzie Morgan <macoafi@gmail.com>
2014-03-26 09:12:30 -0400

    Initial commit of Spree app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Renhab::Application.config.secret_key_base = '71607a5fe779399b0c2490361036ee45e703f1b8f1bb5d1d4d87988f7fec250848dafd6bed29a1dda73ab0037e1eed7e1dc83d6c70b2c6b4ff39ded7d6606572'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Renhab::Application.config.session_store :cookie_store, key: '_renhab_session'
