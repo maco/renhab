00b46bf1a7c566cd82f3325c97efc89dfefaca67
Mackenzie Morgan <macoafi@gmail.com>
2014-09-25 14:49:41 +0000

    set country and update spree version string

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  config.default_country_id = 50
 end
 
 Spree.user_class = "Spree::User"
